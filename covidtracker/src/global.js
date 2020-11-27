98bee2bc1dc9c78a695b7ae899567ef489ec1be7
Khrisna Semara <32382686+Khrisna224@users.noreply.github.com>
2020-11-27 17:21:08 +0800

    fix bug

--- covidtracker/src/global.js
+++ covidtracker/src/global.js
@@ -1,26 +1,34 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 const Global = () => {
-  const [world, setWorld] = useState([])
+  const [confirm, setConfirm] = useState()
+  const [death, setDeath] = useState()
+  const [recovered, setRecovered] = useState()
 
   useEffect(() => {
     axios.get("https://covid19.mathdro.id/api")
-      .then((res) =>{
-        setWorld(res.data)
-        console.log(world)
+      .then((res) => {
+        setConfirm(res.data.confirmed.value)
+        setDeath(res.data.deaths.value)
+        setRecovered(res.data.recovered.value)
       })
       .catch((err) => console.log(err))
   })
 
-  console.log(world)
   const formatter = new Intl.NumberFormat('en')
   return (
     <div className="container">
       <h1>Global</h1>
-      <h1>Confirm: {formatter.format(world.confirmed.value)}</h1>
-      <h1>Recovered: {formatter.format(world.deaths.value)}</h1>
-      <h1>Death: {formatter.format(world.recovered.value)}</h1>
+      <h1>Confirm: </h1>
+      <h1>Recovered: {formatter.format(recovered)}</h1>
+      <h1>Death: {formatter.format(death)}</h1>
+      <div className="card text-white text-center bg-warning w-25">
+        <div className="card-header"><h5 className='card-title'>Confirmed</h5></div>
+        <div className="card-body">
+          <h3 className="card-text">{formatter.format(confirm)}</h3>
+        </div>
+      </div>
     </div>
   );
 }
--- covidtracker/src/indonesia.js
+++ covidtracker/src/indonesia.js
@@ -2,13 +2,18 @@ import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 
 function Indonesia() {
-  const [indo, setIndo] = useState([])
+  const [confirm, setConfirm] = useState()
+  const [death, setDeath] = useState()
+  const [recovered, setRecovered] = useState()
+  const [cases, setCases] = useState()
 
   useEffect(() => {
     axios.get("https://indonesia-covid-19.mathdro.id/api")
       .then((res) =>{
-        setIndo(res.data)
-        console.log(indo)
+        setConfirm(res.data.perawatan)
+        setDeath(res.data.meninggal)
+        setRecovered(res.data.sembuh)
+        setCases(res.data.jumlahKasus)
       })
       .catch((err) => console.log(err))
   })
@@ -17,10 +22,10 @@ function Indonesia() {
   return (
     <div>
       <h1>Indonesia</h1>
-      <h1>Confirm: {formatter.format(indo.perawatan)}</h1>
-      <h1>Recovered: {formatter.format(indo.sembuh)}</h1>
-      <h1>Death: {formatter.format(indo.meninggal)}</h1>
-      <h1>Jumlah kasus: {formatter.format(indo.jumlahKasus)}</h1>
+      <h1>Confirm: {formatter.format(confirm)}</h1>
+      <h1>Recovered: {formatter.format(recovered)}</h1>
+      <h1>Death: {formatter.format(death)}</h1>
+      <h1>Jumlah kasus: {formatter.format(cases)}</h1>
     </div>
   );
 }
