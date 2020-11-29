367bae948a52491d8756f1bab34dc2b5f45258ab
Khrisna Semara <32382686+Khrisna224@users.noreply.github.com>
2020-11-29 17:52:27 +0800

    fix key

--- covidtracker/src/page/countries.js
+++ covidtracker/src/page/countries.js
@@ -19,10 +19,10 @@ const Countries = () => {
       <h2>Country List</h2>
       <div className="row row-cols-1 row-cols-md-4">
         {allCountry.map((country) => (
-          <div className="col mb-4">
+          <div className="col mb-4" key={country.name}>
             <div className="card text-dark text-center">
               <div className="card-header">
-                <h5 className="card-title" key={country.name}>
+                <h5 className="card-title">
                   <Link to={`/country/${country.name}`}>{country.name}</Link>
                 </h5>
               </div>
